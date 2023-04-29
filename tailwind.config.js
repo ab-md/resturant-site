193d3f342feef1f0e70ffa73c6affa198cba5ae4
Abolfazl Madadi <metafite800@gmail.com>
2023-04-30 01:06:42 +0430

    Fix tailwindCss's bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: 20,
-    },
     extend: {
       borderWidth: {
-        3: "3px",
-        5: "5px"
+        3: "3px;",
+        5: "5px;"
       },
       boxShadow: {
-        my: "rgba(92, 245, 115, 0.123) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;",
+        "my": "rgba(92, 245, 115, 0.123) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px"
       },
     },
   },
